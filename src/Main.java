d9663b59c8c5093043f2336602eefc66ce52d315
Emilio <25420014@uttcampus.edu.mx>
2026-07-03 13:21:52 -0600

    Agregue un comentario en la linea 10

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ void main() {
         IO.println("i = " + i);
         //hola emilio
         //ala que loco tio
-        System.out.println("momilio es gay, y lo amo, uwuuwuwuwu"); //voy agregar datos
+        System.out.println("momilio es gay, y lo amo, uwuuwuwuwu"); //waos
     }
 }
